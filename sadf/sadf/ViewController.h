ff9844dcaf79d2b0ac6f40b39063291254579c10
wanglei0918 <wanglei@wind4app.com>
2015-10-26 16:43:16 +0800

    1.0

---
+++ sadf/sadf/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sadf
+//
+//  Created by fdkj0001 on 15/10/26.
+//  Copyright © 2015年 fdkj0001. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ sadf/sadf/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  sadf
+//
+//  Created by fdkj0001 on 15/10/26.
+//  Copyright © 2015年 fdkj0001. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
